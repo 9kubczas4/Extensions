942c756d8e6aa4fe815fd41da77f36d32902e296
Pawel Kubiak <pawel.kubiak@ormlondon.com>
2019-01-09 13:06:01 +0100

    CreateListOfType extension method for Type

---
+++ Extensions/TypeExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Extensions
+{
+    public static class TypeExtensions
+    {
+        public static IList CreateListOfType(this Type type)
+        {
+            var genericList = typeof(List<>).MakeGenericType(type);
+            return Activator.CreateInstance(genericList) as IList;
+        }
+    }
+}
---
+++ ExtensionsTests/TypeExtensionsTest.cs
@@ -0,0 +1,39 @@
+﻿using Extensions;
+using NUnit.Framework;
+using System;
+using System.Collections;
+
+namespace ExtensionsTests
+{
+    [TestFixture]
+    public class TypeExtensionsTest
+    {
+        [Test]
+        public void CreateListOfType_WhenProvidedIsSimpleType_ShouldCreateNewList()
+        {
+            // arrange
+            int i = 0;
+            Type type = i.GetType();
+
+            // act
+            IList result = type.CreateListOfType();
+
+            // assert
+            Assert.IsNotInstanceOf(type, result);
+        }
+
+        [Test]
+        public void CreateListOfType_WhenProvidedIsNotSimpleType_ShouldCreateNewList()
+        {
+            // arrange
+            String str = "text";
+            Type type = str.GetType();
+
+            // act
+            IList result = type.CreateListOfType();
+
+            // assert
+            Assert.IsNotInstanceOf(type, result);
+        }
+    }
+}
